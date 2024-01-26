b393429fa3e1451f29190dac782fbf816fa05ee3
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2024-01-26 21:14:43 +0000

    Update MainActivityV2.kt

--- app/src/main/java/com/huanchengfly/tieba/post/MainActivityV2.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/MainActivityV2.kt
@@ -105,7 +105,6 @@ import com.huanchengfly.tieba.post.utils.newIntentFilter
 import com.huanchengfly.tieba.post.utils.registerPickMediasLauncher
 import com.huanchengfly.tieba.post.utils.requestIgnoreBatteryOptimizations
 import com.huanchengfly.tieba.post.utils.requestPermission
-import com.microsoft.appcenter.analytics.Analytics
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
@@ -402,17 +401,6 @@ class MainActivityV2 : BaseComposeActivity() {
 
                 navController.navigatorProvider += navigator
 
-                LaunchedEffect(currentDestination) {
-                    val curDest = currentDestination
-                    if (curDest != null) {
-                        Analytics.trackEvent(
-                            "PageChanged",
-                            mapOf(
-                                "page" to curDest.route,
-                            )
-                        )
-                    }
-                }
 
                 CompositionLocalProvider(
                     LocalNavController provides navController,

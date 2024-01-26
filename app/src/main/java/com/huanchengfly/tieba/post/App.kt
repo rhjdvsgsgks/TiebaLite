568731632dacaf7093de1a6841fce0333230765c
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2024-01-26 20:56:44 +0000

    remove appcenter analytic / update checking

--- app/src/main/java/com/huanchengfly/tieba/post/App.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/App.kt
@@ -54,14 +54,6 @@ import com.huanchengfly.tieba.post.utils.appPreferences
 import com.huanchengfly.tieba.post.utils.applicationMetaData
 import com.huanchengfly.tieba.post.utils.launchUrl
 import com.huanchengfly.tieba.post.utils.packageInfo
-import com.microsoft.appcenter.AppCenter
-import com.microsoft.appcenter.analytics.Analytics
-import com.microsoft.appcenter.crashes.Crashes
-import com.microsoft.appcenter.distribute.Distribute
-import com.microsoft.appcenter.distribute.DistributeListener
-import com.microsoft.appcenter.distribute.ReleaseDetails
-import com.microsoft.appcenter.distribute.UpdateAction
-import com.microsoft.appcenter.distribute.UpdateTrack
 import dagger.hilt.android.HiltAndroidApp
 import net.swiftzer.semver.SemVer
 import org.litepal.LitePal
@@ -128,15 +120,6 @@ class App : Application(), IApp, SketchFactory {
         LitePal.initialize(this)
         AccountUtil.init(this)
         Config.init(this)
-        val isSelfBuild = applicationMetaData.getBoolean("is_self_build")
-        if (!isSelfBuild) {
-            Distribute.setUpdateTrack(if (appPreferences.checkCIUpdate) UpdateTrack.PRIVATE else UpdateTrack.PUBLIC)
-            Distribute.setListener(MyDistributeListener())
-            AppCenter.start(
-                this, "b56debcc-264b-4368-a2cd-8c20213f6433",
-                Analytics::class.java, Crashes::class.java, Distribute::class.java
-            )
-        }
         setIcon()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         ThemeUtils.init(ThemeDelegate)
@@ -244,39 +227,6 @@ class App : Application(), IApp, SketchFactory {
         var DENSITY = 0f
     }
 
-    class MyDistributeListener : DistributeListener {
-        override fun onReleaseAvailable(
-            activity: Activity,
-            releaseDetails: ReleaseDetails
-        ): Boolean {
-            val versionName = releaseDetails.shortVersion
-            val newSemVer = SemVer.parse(versionName)
-            val currentSemVer = SemVer.parse(BuildConfig.VERSION_NAME)
-            if (newSemVer <= currentSemVer) {
-                return true
-            }
-            val releaseNotes = releaseDetails.releaseNotes
-            if (activity is BaseActivity) {
-                activity.showDialog {
-                    setTitle(activity.getString(R.string.title_dialog_update, versionName))
-                    setMessage(releaseNotes)
-                    setCancelable(!releaseDetails.isMandatoryUpdate)
-                    setPositiveButton(R.string.appcenter_distribute_update_dialog_download) { _, _ ->
-                        Distribute.notifyUpdateAction(UpdateAction.UPDATE)
-                    }
-                    if (!releaseDetails.isMandatoryUpdate) {
-                        setNeutralButton(R.string.appcenter_distribute_update_dialog_postpone) { _, _ ->
-                            Distribute.notifyUpdateAction(UpdateAction.POSTPONE)
-                        }
-                        setNegativeButton(R.string.button_next_time, null)
-                    }
-                }
-            }
-            return true
-        }
-
-        override fun onNoReleaseAvailable(activity: Activity) {}
-    }
 
     companion object {
         const val TAG = "App"
